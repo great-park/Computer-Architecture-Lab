951e77c6728c9615867c0cdf7b5ac80cc77dbf08
great-park <cksgh1735@naver.com>
2022-10-05 01:09:02 +0900

    fix

--- Lab1/lab1.s
+++ Lab1/lab1.s
@@ -1,14 +1,16 @@
 main:
     addiu $s0, $0, 10
-    addiu $s1, $0, 4
+    addiu $s1, $0, 3
 
 for:
     slt $t1, $s1, $s0
-    beq $t1, $0, done
+    beq $t1, $0, set
     nop
     subu $s0, $s0, $s1
     j for
     nop
 
-done:
+set:
     addu $s2, $0, $s0
+
+done:
