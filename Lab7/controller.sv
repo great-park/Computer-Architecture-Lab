49bf7eedfed4df7b8ff3320c0d8918bb24af6767
great-park <cksgh1735@naver.com>
2022-12-06 12:47:50 +0900

    fix: controller module

--- Lab7/controller.sv
+++ Lab7/controller.sv
@@ -5,6 +5,18 @@ module controller(
   output logic [2:0] oALUControl
 );
 
+always@(iOp) begin
+  oRegWrite = 1'b0;
+  oMemWrite = 1'b0;
+  oALUControl = 3'b00;
+
+  if (iOp == 6'h23) begin
+    oMemWrite = 1'b0;
+    oRegWrite = 1'b1;
+    oALUControl = 3'b010;
+  end
+end
+
 
 endmodule
  
