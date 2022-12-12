a01c89960d34348f7404b822753b0b53622a290f
great-park <cksgh1735@naver.com>
2022-12-13 00:34:29 +0900

    feat: implement R type instruction

--- Lab9/controller.sv
+++ Lab9/controller.sv
@@ -1,24 +1,45 @@
 module controller(
   input logic [5:0] iOp,
+  input logic [5:0] iFunct,
   output logic oRegWrite,
   output logic oMemWrite,
+  output logic oRegDst,
+  output logic oALUSrc,
+  output logic oMemtoReg,
   output logic [2:0] oALUControl
 );
   logic [1:0] aluop;
   
   always_comb
     case(iOp)
+      6'b000000: begin
+        oRegDst = 1'b1;
+        oALUSrc = 1'b0;
+        oMemtoReg = 1'b0;
+        oRegWrite = 1'b1;
+        oMemWrite = 1'b0;
+        aluop = 2'b10;
+      end
       6'b100011: begin
+        oRegDst = 1'b0;
+        oALUSrc = 1'b1;
+        oMemtoReg = 1'b1;
         oRegWrite = 1'b1;
         oMemWrite = 1'b0;
         aluop = 2'b00;
       end
       6'b101011: begin
+        oRegDst = 1'b0;
+        oALUSrc = 1'b1;
+        oMemtoReg = 1'b0;
         oRegWrite = 1'b0;
         oMemWrite = 1'b1;
         aluop = 2'b00;
       end
       default: begin
+        oRegDst = 1'b0;
+        oALUSrc = 1'b0;
+        oMemtoReg = 1'b0;
         oRegWrite = 1'b0;
         oMemWrite = 1'b0;
         aluop = 2'b00;
@@ -27,7 +48,16 @@ module controller(
   
   always_comb
     case(aluop)
-      2'b00: oALUControl = 3'b010;
+      2'b00: oALUControl = 3'b010; //add
+      2'b10: begin
+        case(iFunct)
+          6'b100000: oALUControl = 3'b010; //add
+          6'b100010: oALUControl = 3'b110; //subtract
+          6'b100100: oALUControl = 3'b000; //and
+          6'b100101: oALUControl = 3'b001; //or
+          6'b101010: oALUControl = 3'b111; //slt
+        endcase
+      end
       default: oALUControl = 3'b000;
     endcase
 endmodule
--- Lab9/datapath.sv
+++ Lab9/datapath.sv
@@ -3,6 +3,8 @@
 `include "imem.sv"
 `include "dmem.sv"
 `include "controller.sv"
+`include "mux5.sv"
+`include "mux32.sv"
 
 module mips(
   input logic iClk,
@@ -18,7 +20,13 @@ module mips(
   
   logic CTL_RegWrite;
   logic CTL_MemWrite;
+  logic CTL_RegDst;
+  logic CTL_ALUSrc;
+  logic CTL_MemtoReg;
   logic [2:0] CTL_ALUControl;
+  logic [4:0] WriteReg;
+  logic [31:0] SrcB;
+  logic [31:0] Result;
   
   alu ALU(
     .iA		(REG_SrcA),
@@ -33,9 +41,9 @@ module mips(
     .iReset	(iReset),
     .iRaddr1(IMEM_Inst[25:21]),
     .iRaddr2(IMEM_Inst[20:16]),
-    .iWaddr	(IMEM_Inst[20:16]),
+    .iWaddr	(WriteReg),
     .iWe	(CTL_RegWrite),
-    .iWdata	(DMEM_ReadData),
+    .iWdata	(Result),
     .oRdata1(REG_SrcA),
     .oRdata2(REG_WriteData)
   );
@@ -55,11 +63,40 @@ module mips(
   
   controller CTL(
     .iOp		(IMEM_Inst[31:26]),
+    .iFunct (IMEM_Inst[5:0]),
     .oRegWrite	(CTL_RegWrite),
     .oMemWrite	(CTL_MemWrite),
+    .oRegDst(CTL_RegDst),
+    .oALUSrc(CTL_ALUSrc),
+    .oMemtoReg(CTL_MemtoReg),
     .oALUControl(CTL_ALUControl)
   );
-  
+
+  assign WriteReg = CTL_RegDst ? IMEM_Inst[15:11] : IMEM_Inst[20:16];
+  assign SrcB = CTL_ALUSrc ? IMEM_Inst : REG_WriteData;
+  assign Result = CTL_MemtoReg ? DMEM_ReadData : ALU_ALUResult;
+
+  // mux5 MUX5(
+  //   .in0(IMEM_Inst[20:16]),
+  //   .in1(IMEM_Inst[15:11]),
+  //   .sel(CTL_RegDst),
+  //   .out(WriteReg)
+  // );
+
+  // mux32 MUX32a(
+  //   .in0(REG_WriteData),
+  //   .in1(IMEM_Inst),
+  //   .sel(CTL_ALUSrc),
+  //   .out(SrcB)
+  // );
+
+  // mux32 MUX32b(
+  //   .in0(ALU_ALUResult),
+  //   .in1(DMEM_ReadData),
+  //   .sel(CTL_MemtoReg),
+  //   .out(Result)
+  // );
+
   always_ff@(posedge iClk, posedge iReset)
     if(iReset)
       pc <= 0;
