9891ab3c8199e73826958096dcf9bb3f4086cb5a
great-park <cksgh1735@gmail.com>
2022-12-21 01:58:07 +0900

    fix: register slice for Fetch and Decode stage

--- Lab10/datapath.sv
+++ Lab10/datapath.sv
@@ -9,36 +9,61 @@ module mips(
   input logic iReset
 );
 
-  logic [31:0] ALU_ALUResult;
-  logic [31:0] REG_SrcA;
-  logic [31:0] REG_WriteData;
+
+
   logic [31:0] IMEM_InstF;
   logic [31:0] IMEM_InstD;
-  logic [31:0] DMEM_ReadData;
+  logic [31:0] REG_SrcAD;
+  logic [31:0] REG_SrcAE;
+  logic [15:0] SignImmD;
+  logic [15:0] SignImmE;
+  logic [4:0] RtD;
+  logic [4:0] RdD;
+  logic [4:0] RtE;
+  logic [4:0] RdE;
+
+
+
+  logic [31:0] ALU_ALUResultE;
+  logic [31:0] ALU_ALUResultM;
+  logic [31:0] SrcBE;
+  logic [31:0] REG_WriteDataD;
+  logic [31:0] REG_WriteDataE;
+  logic [4:0] WriteRegE;
+  logic [4:0] WriteRegM;
+  logic [4:0] WriteRegW;
+  logic [31:0] DMEM_ReadDataM;
+  logic [31:0] DMEM_ReadDataW;
+
   logic [31:0] pc;
-  logic [4:0] WriteReg;
-  logic [31:0] SrcB;
   logic [31:0] Result;
-  logic [15:0] SignImm;
-  logic [4:0] Rt;
-  logic [4:0] Rd;
+
+
+  logic CTL_ALUSrcD;
+  logic CTL_ALUSrcE;
+
+  logic [2:0] CTL_ALUControlD;
+  logic [2:0] CTL_ALUControlE;
   
   logic CTL_RegWrite;
   logic CTL_MemWrite;
   logic CTL_RegDst;
-  logic CTL_ALUSrc;
-  logic CTL_MemtoReg;
-  logic [2:0] CTL_ALUControl;
   
-  assign Rt = IMEM_InstD[20:16];
-  assign Rd = IMEM_InstD[15:11];
+  logic CTL_MemtoRegD;
+  logic CTL_MemtoRegE;
+  logic CTL_MemtoRegM;
+  logic CTL_MemtoRegW;
+  
+  
+  assign RtD = IMEM_InstD[20:16];
+  assign RdD = IMEM_InstD[15:11];
   assign WriteReg = CTL_RegDst ? Rd : Rt;
-  assign SignImm = {{16{IMEM_InstD[15]}}, IMEM_InstD[15:0]};
-  assign SrcB = CTL_ALUSrc ? SignImm : REG_WriteData;
+  assign SignImmD = {{16{IMEM_InstD[15]}}, IMEM_InstD[15:0]};
+  assign SrcB = CTL_ALUSrc ? SignImmD : REG_WriteData;
   assign Result = CTL_MemtoReg ? DMEM_ReadData : ALU_ALUResult;
   
   alu ALU(
-    .iA		(REG_SrcA),
+    .iA		(REG_SrcAE),
     .iB		(SrcB),
     .iF		(CTL_ALUControl),
     .oY		(ALU_ALUResult),
@@ -53,8 +78,8 @@ module mips(
     .iWaddr	(WriteReg),
     .iWe	(CTL_RegWrite),
     .iWdata	(Result),
-    .oRdata1(REG_SrcA),
-    .oRdata2(REG_WriteData)
+    .oRdata1(REG_SrcAD),
+    .oRdata2(REG_WriteDataD)
   );
   
   imem IMEM(
@@ -91,8 +116,18 @@ module mips(
     always_ff@(posedge iClk, posedge iReset)
       if(iReset) begin
       	  IMEM_InstD <= 0;
+          REG_SrcAE <= 0;
+          REG_WriteDataE <= 0;
+          RtE <= 0;
+          RdE <= 0;
+          SignImmE <= 0;
         end else begin 
           IMEM_InstD <= IMEM_InstF;
+          REG_SrcAE <= REG_SrcAD;
+          REG_WriteDataE <= REG_WriteDataD;
+          RtE <= RtD;
+          RdE <= RdD;
+          SignImmE <= SignImmD;
         end
 
 endmodule
